b76021784b2ece027b50e55dcd16bdd40f2662a8
Sygmei <3835355+Sygmei@users.noreply.github.com>
2023-11-14 02:05:40 +0100

    Update depsolver.py

--- depsolver.py
+++ depsolver.py
@@ -80,26 +80,3 @@ def resolve_dependencies(function: Callable, **kwargs):
 
 def depends(function: Callable):
     return Dependency(function)
-
-def requires_ten() -> int:
-    print("Resolve requires_ten")
-    return 10
-
-def requires_twenty(ten: int = depends(requires_ten)):
-    print("Resolve requires_twenty")
-    return ten * 2
-
-def requires_twenty_plus_a_plus_b(a: int, b: int, twenty: int = depends(requires_twenty)):
-    print("Resolve requires_twenty_plus_a_plus_b")
-    return twenty + a + b
-
-def requires_multiple(a, b, complex: int = depends(requires_twenty_plus_a_plus_b), ten1: int = depends(requires_ten), ten2: int = depends(requires_ten), twenty1: int = depends(requires_twenty), twenty2: int = depends(requires_twenty)):
-    print("Resolve requires_multiple", a, b, complex, ten1, ten2, twenty1, twenty2)
-    return a + b + complex + ten1 + ten2 + twenty1 + twenty2
-
-def main():
-    result = resolve_dependencies(requires_multiple, a=50, b=30)
-    print(result)
-
-if __name__ == '__main__':
-    main()
